2df214916d1590603beec1704e8fd09025fe3ff1
Clint <clinthiles1992@gmail.com>
2025-01-18 10:16:10 -0800

    Add dialpad button to Footer and update styles; increase dialpad size

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Styles/Footer.css";
+// import dialpadIcon from "../../public/emblems/dialpad_24dp_75FB4C_FILL0_wght400_GRAD0_opsz24.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <button className="footer-button">📞</button>
       <button className="footer-button">👤</button>
+      <button className="footer-button">
+        <img
+          className="footer-button"
+          id="dialpad"
+          src="public/emblems/dialpad_20dp_FFFFFF_FILL0_wght400_GRAD0_opsz20.png"
+          alt="dialpad"
+        />
+      </button>
+      <button className="footer-button">⚙️</button>
       <button className="footer-button">⚙️</button>
     </footer>
   );
