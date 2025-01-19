d71ebb9b1a5d1a4684e5ac1a8337723fe7e6318d
Clint <clinthiles1992@gmail.com>
2025-01-19 11:27:23 -0800

    Add CallItem and TabBar components; implement call archiving functionality and associated styles

---
+++ src/Components/CallItem.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const CallItem = ({ call, onToggleArchive, getCallIcon }) => {
+  return (
+    <div className="call-log-item">
+      <div className="call-icon-wrapper">{getCallIcon(call)}</div>
+      <div className="call-details">
+        <div className="number">+{call.from}</div>
+        <div className="details">tried to call on {call.to}</div>
+      </div>
+      <div className="archive-button">
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            onToggleArchive(call.id);
+          }}
+        >
+          <img
+            src="public/emblems/verticalDots.png"
+            alt={call.is_archived ? "Unarchive" : "Archive"}
+            className="archive-icon"
+          />
+        </button>
+      </div>
+      <div className="time">
+        {new Date(call.created_at).toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CallItem;
---
+++ src/Components/TabBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const TabBar = ({ activeTab, setActiveTab, onArchiveAll, onUnarchiveAll }) => {
+  return (
+    <div className="tabs">
+      <div className="act-arch-btns">
+        <button onClick={() => setActiveTab("activity")}>Activity Feed</button>
+        <button onClick={() => setActiveTab("archived")}>Archived</button>
+      </div>
+
+      <div className="actions">
+        {activeTab === "activity" ? (
+          <button className="archive-all" onClick={onArchiveAll}>
+            <img className="button-icon" src="public/emblems/archive.png" alt="activity" />
+            Archive all calls
+          </button>
+        ) : (
+          <button className="archive-all" onClick={onUnarchiveAll}>
+            <img className="button-icon" src="public/emblems/archive.png" alt="activity" />
+            Unarchive All
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TabBar;
