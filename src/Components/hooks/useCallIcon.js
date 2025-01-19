dce1e353593bd12d35837d1816315e759ee0ccf0
Clint <clinthiles1992@gmail.com>
2025-01-19 12:21:13 -0800

    Add comments to useCallIcon hook for improved code clarity

--- src/Components/hooks/useCallIcon.js
+++ src/Components/hooks/useCallIcon.js
@@ -1,6 +1,8 @@
-import { FaPhoneVolume, FaPhone, FaPhoneSlash } from "react-icons/fa";
+import { FaPhoneVolume, FaPhone, FaPhoneSlash } from "react-icons/fa"; // Awesome npm package
 
+// This determins which phone icon to display :)
 export const useCallIcon = () => {
+  // takes the call object and returns the right icon
   const getCallIcon = (call) => {
     if (call.call_type === "missed") {
       return <FaPhoneSlash className="missed-call-icon" />;
